e8070a167ad0c7ef9a872c8f8c906c4b9758a63a
Kirill Timofeev <Kirill.Timofeev@jetbrains.com>
2018-10-08 12:05:55 +0300

    add test

--- src/test/Test.kt
+++ src/test/Test.kt
@@ -25,4 +25,23 @@ class LCTest {
     cut(vvv[5], vvv[4])
     check(!isConnected(vvv[4], vvv[5]))
   }
+
+  @Test
+  fun test2() {
+    val v = Array(10) { Node() }
+    link(v[0], v[1])
+    link(v[2], v[3])
+    link(v[2], v[4])
+    check(isConnected(v[3], v[4]))
+    check(isConnected(v[2], v[4]))
+    check(!isConnected(v[0], v[4]))
+    link(v[0], v[2])
+    check(isConnected(v[1], v[2]))
+    check(isConnected(v[0], v[4]))
+    check(isConnected(v[3], v[0]))
+    cut(v[2], v[0])
+    check(!isConnected(v[1], v[2]))
+    check(!isConnected(v[0], v[4]))
+    check(!isConnected(v[3], v[0]))
+  }
 }
